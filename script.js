2a5388154e00532ac943190083268784b5a1218c
3Melody <plugeiei2003@gmail.com>
2024-04-10 17:21:45 +0700

    final-commit

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ document.getElementById("close-nav").addEventListener("click", function() {
         navbarShow.style.display = 'none';
       }
 })
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
